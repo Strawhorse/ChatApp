a2be09aa22b30c2d712b059902949da3866d724c
Sean B <strawhorse@gmail.com>
2024-02-28 21:00:40 +0000

    Creating input for handle from user
    Later can add some validation to the user if the nickname is already used etc.

--- ChatApp/src/Server.java
+++ ChatApp/src/Server.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -29,10 +32,42 @@ public class Server implements Runnable {
 
     class ConnectionHandler implements Runnable{
 
+        private Socket client;
+
+//        Bufferedreader to get the stream from the socket
+        private BufferedReader in;
+
+//        Printwriter when writing to the client
+        private PrintWriter out;
+
+        private String nickname;
+
+//        create constructor which takes in the socket as a parameter
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
+
         @Override
         public void run() {
 
             //        new connection handler for each client that connects
+
+//            initialise reader and writer
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+//                now we can send something to the client
+                out.println("Please enter your handle: ");
+                nickname = in.readLine();
+
+//                can perform checks on the nickname entered here ...
+                
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
 
         }
