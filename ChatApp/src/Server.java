1aff467c870978b54c0e7460bb1bead61501926d
Sean B <strawhorse@gmail.com>
2024-02-28 20:32:04 +0000

    Set up server class with runnable interface, make an inner class to handle each connection to the server

--- ChatApp/src/Server.java
+++ ChatApp/src/Server.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server implements Runnable {
 
@@ -5,5 +8,33 @@ public class Server implements Runnable {
     @Override
     public void run() {
 
+//        server will constantly listen for incoming connections and then accept them
+
+        try {
+            ServerSocket server = new ServerSocket(9999);
+
+//            server has the accept method which - returns a client socket
+            Socket client = server.accept();
+
+
+//            still need a shutdown function here
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    class ConnectionHandler implements Runnable{
+
+        @Override
+        public void run() {
+
+            //        new connection handler for each client that connects
+
+
+        }
     }
 }
